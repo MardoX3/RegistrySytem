91485fc087fba5c084fa1b4ace797b1212532ca9
Administrator <wiktorprzylucki3@wp.pl>
2023-12-02 20:47:03 +0100

    v 2.11

--- src/main/java/pl/gornik/RegistrySystem.java
+++ src/main/java/pl/gornik/RegistrySystem.java
@@ -7,7 +7,7 @@ import pl.gornik.users.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class Main {
+public class RegistrySystem {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
